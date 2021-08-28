0a3bd9914a83e84f738b0371a144b8f49480ca90
javaprogramozas <javaprogramozas.hu@gmail.com>
2021-08-28 17:49:37 +0200

    [v9] Változók

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java-reprogramozas
 A Java programozás sorozat demó kódjai
+
+Lejátszási lista: https://www.youtube.com/playlist?list=PL45FGlNxfdFS4MzTfG4XXWqpXuzA7wgMq
---
+++ src/v9/Változók.java
@@ -0,0 +1,38 @@
+package v9;
+
+public class Változók {
+
+    public static void main(String[] args) {
+        // Deklarációs utasítás
+        int életkor;
+
+        // Deklarációs utasítás kezdőértékkel
+        String név = "Molnár Zoltán";
+
+        // Értékadó utasítás
+        életkor = 18;
+        név = "Nagy Károly";
+
+        // Deklaráció több változóval
+        int x, y, z;
+
+        // Kevert mód
+        double alma = 5.1, körte;
+
+        // Kis és nagybetű érzékeny
+        short változó;
+        short VÁLTOZÓ;
+        short vÁlToZó;
+
+        // Egyéb
+        int BETŰK_SZÁMA = 44, sz4m1sl3h3t, $, $fura$, _jajj_, camelCase;
+
+        // érvénytelen
+        //int _, true, double, for, while, 1szám;
+
+        // deklarált változó nem deklarálható újra
+        //String életkor;
+
+    }
+
+}
