27231cb03e3f6537b716fffc0bc0bc3fd483f88f
javaprogramozas <javaprogramozas.hu@gmail.com>
2022-04-28 19:07:41 +0200

    [v63] Kivételkezelés - II. rész

--- src/v62/ExceptionHandling.java
+++ src/v62/ExceptionHandling.java
@@ -1,31 +1,69 @@
 package v62;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Scanner;
 
 public class ExceptionHandling {
 
     public static void main(String[] args) {
-        tryCatchFinallyBlock();
+        try {
+            String text = catchOrThrow("valami.txt");
+            System.out.println(text);
+        } catch (FileNotFoundException e) {
+            System.out.println("A megadott fájl nem létezik!");
+        }
     }
 
     private static void tryCatchFinallyBlock() {
-        Scanner input = new Scanner(System.in);
-        do {
-            try {
-                System.out.print("Adj meg egy számot: ");
-                int number = Integer.parseInt(input.nextLine());
-                int result = 100 / number;
-                System.out.printf("100 / %d = %d%n", number, result);
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Nem megfelelő input");
-            } catch (ArithmeticException e) {
-                System.out.println("0-val nem lehet osztani");
-                break;
-            }
-        } while (true);
-        input.close();
+        try (Scanner input = new Scanner(System.in)) {
+            do {
+                try {
+                    System.out.print("Adj meg egy számot: ");
+                    int number = Integer.parseInt(input.nextLine());
+                    int result = 100 / number;
+                    System.out.printf("100 / %d = %d%n", number, result);
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Nem megfelelő input");
+                } catch (ArithmeticException e) {
+                    System.out.println("0-val nem lehet osztani");
+                    break;
+                }
+            } while (true);
+        }
         System.out.println("Metódus vége");
+    }
+
+    private static String catchOrThrow(String filename) throws FileNotFoundException {
+        FileReader reader = new FileReader(filename);
+        try {
+            StringWriter writer = new StringWriter();
+            reader.transferTo(writer);
+            return writer.toString();
+        } catch (IOException e) {
+            return "";
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException ignored) {}
+            }
+        }
+    }
+
+    private static String tryWithResources(String filename) {
+        try (FileReader reader = new FileReader(filename);
+             StringWriter writer = new StringWriter()) {
+            reader.transferTo(writer);
+            return writer.toString();
+        } catch (IOException e) {
+            return "";
+        } finally {
+            System.out.println("Végre vége..");
+        }
     }
 
 }
